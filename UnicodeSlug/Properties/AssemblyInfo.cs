351999582b05ca27571e46e93ecbff3e3b4bd1ef
Fabien <fabienbrooke@gmail.com>
2015-01-03 17:36:30 -0800

    add nuspec file

--- UnicodeSlug/Properties/AssemblyInfo.cs
+++ UnicodeSlug/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("UnicodeSlug")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A permissive slug generator that works with unicode.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("UnicodeSlug")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
